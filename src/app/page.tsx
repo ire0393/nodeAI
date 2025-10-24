7c9ac2bf21902b5f449845df039ca6668b0ce586
Larry <ainairemide@gmail.com>
2025-10-24 14:27:29 +0100

    installationa nd dependecies

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { Button} from "@/components/ui/button"
+
+const Page = () => {
+  return(
+    <div className="text-red-500">
+     Hello World
+     <Button>
+      Click me
+     </Button>
+    </div>
+)
+
+  }
+
+export default Page
+
+  
+
+
+
+
+
+
